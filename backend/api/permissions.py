1b465094f13698f9c6319e225d1790da81394114
tWoAlex <thewoalex@gmail.com>
2023-06-26 23:09:08 +0500

    Fixed permissions and settings

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -9,19 +9,19 @@ class IsAuthorOrReadOnly(permissions.IsAuthenticatedOrReadOnly):
         is_author = (obj.author == user
                      if user.is_authenticated
                      else False)
-        return is_author or request.method == 'GET'
+        return is_author or request.method in permissions.SAFE_METHODS
 
 
 class IsActiveOrReadOnly(permissions.IsAuthenticatedOrReadOnly):
     message = 'Вы заблокированы'
 
-    def __is_active__(self, request):
+    def __is_active(self, request):
         return (not request.user.is_blocked
                 if request.user.is_authenticated
                 else True)
 
     def has_permission(self, request, view):
-        return self.__is_active__(request) or request.method == 'GET'
+        return self.__is_active(request) or request.method == 'GET'
 
     def has_object_permission(self, request, view, obj):
-        return self.__is_active__(request) or request.method == 'GET'
+        return self.__is_active(request) or request.method == 'GET'
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -5,9 +5,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'yoursecretkey')
 
-DEBUG = False
+DEBUG = os.getenv('DEBUG', False)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', ['*'])
 
 
 DJANGO_APPS = [
